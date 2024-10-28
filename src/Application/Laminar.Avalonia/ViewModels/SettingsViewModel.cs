53eebdff1ed269fbfdd099d81f8df9ffbf6b7ea4
Adam-Wilkinson <adwilko101@gmail.com>
2024-10-28 01:50:56 +0000

    Animation duration and font size are now in settings. Custom fonts also work now but I don't like any of them.

--- src/Application/Laminar.Avalonia/ViewModels/SettingsViewModel.cs
+++ src/Application/Laminar.Avalonia/ViewModels/SettingsViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia;
 using Avalonia.Styling;
 using CommunityToolkit.Mvvm.ComponentModel;
@@ -7,6 +8,7 @@ namespace Laminar.Avalonia.ViewModels;
 public partial class SettingsViewModel : ViewModelBase
 {
     [ObservableProperty] private string _theme = nameof(ThemeVariant.Default);
+    [ObservableProperty] private double _fontSizeScale = 1.0;
 
     public SettingsViewModel()
     {
@@ -18,6 +20,22 @@ public partial class SettingsViewModel : ViewModelBase
     public string[] Themes { get; } =
         [nameof(ThemeVariant.Default), nameof(ThemeVariant.Dark), nameof(ThemeVariant.Light)];
 
+    public double TransitionDuration
+    {
+        get => Application.Current is not null 
+               && Application.Current.Resources.TryGetResource("AnimationDuration", Application.Current.ActualThemeVariant, out var durationObject) 
+               && durationObject is TimeSpan duration
+            ? duration.TotalMilliseconds
+            : default;
+        set
+        {
+            if (Application.Current is not null)
+            {
+                Application.Current.Resources["AnimationDuration"] = TimeSpan.FromMilliseconds(value);
+            }
+        }
+    }
+    
     partial void OnThemeChanged(string value)
     {
         if (Application.Current is null) return;
@@ -28,5 +46,15 @@ public partial class SettingsViewModel : ViewModelBase
             nameof(ThemeVariant.Dark) => ThemeVariant.Dark,
             _ => ThemeVariant.Default,
         };
+    }
+
+    partial void OnFontSizeScaleChanged(double value)
+    {
+        if (Application.Current is null) return;
+        
+        Application.Current.Resources["H1FontSize"] = value * 34;
+        Application.Current.Resources["H2FontSize"] = value * 24;
+        Application.Current.Resources["B1FontSize"] = value * 18;
+        Application.Current.Resources["B2FontSize"] = value * 14;
     }
 }
